3f4856a9fca55602027c8622c0ea7b3d3e8d0bbf
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-28 15:34:05 +0900

    Add table view

---
+++ NewsReader/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by yukimasa ikeda on 2019/07/28.
+//  Copyright © 2019 yukimasa. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
