7742dd10de45346a997d6f06d9203a4e6adab34f
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-28 15:44:39 +0900

    Add comment out

--- NewsReader/ListViewController.swift
+++ NewsReader/ListViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
+    // セルの数を設定する
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
+    // セルの内容を設定する
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // セルを再利用する
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         return cell
     }
